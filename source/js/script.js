56cac49e34c8eb1b4ae5c3dbbad8b7fd752b6d67
Maxim Shashurin <waiyyshik@gmail.com>
2021-04-15 17:58:32 +0500

    Исправляет мобильное меню

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,10 @@
 const navToggle = document.querySelector(".nav-toggle");
 const mainNav = document.querySelector(".main-nav");
 
+mainNav.classList.add("visually-hidden");
+
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav-toggle__open");
-  mainNav.classList.toggle("main-nav__open");
+  mainNav.classList.toggle("visually-hidden");
 });
 //nav-toggle close/open
